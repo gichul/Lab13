391c1d2a3f17996d27e3b2fe452fdee4c58f62b0
kim gi chul <21500048@handong.edu>
2018-05-01 18:28:32 +0900

    finish prob1

--- src/main/java/edu/handong/csee/java/lab13/prob1/Friend.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Friend.java
@@ -1,8 +1,8 @@
-package edu.handong.csee.java.lab13.prob1;
+package edu.handong.csee.java.lab13.prob1;//package name
 
-public class Friend {
-	public void justFriend() {
-		System.out.println("Just Friend!");
+public class Friend {   //declare class Friend
+	public void justFriend() {	//declare method named justFriend
+		System.out.println("Just Friend!");  //statement that print the sentence on the console
 	}
 	
 
